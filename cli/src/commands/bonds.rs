0e50750d4946d68b2e97c776dea81fbef853dfb8
TWloYWkgQm9kbmFyaXU= <mihai.bodnariu@butonium.com>
2024-02-15 14:05:09 +0200

    fix: apply chalda's review

--- cli/src/commands/bonds.rs
+++ cli/src/commands/bonds.rs
@@ -1,16 +1,14 @@
 use crate::commands::common::CommonCollectOptions;
 use crate::{dto::ValidatorBondRecord, utils::rpc::get_rpc_client};
-use anchor_lang::AccountDeserialize;
+use anchor_lang::{AccountDeserialize, Discriminator};
 use serde_yaml;
 use solana_account_decoder::UiAccountEncoding;
 use solana_client::{
     rpc_config::{RpcAccountInfoConfig, RpcProgramAccountsConfig},
     rpc_filter::Memcmp,
 };
+use validator_bonds::constants::PROGRAM_ID;
 use validator_bonds::state::bond::Bond;
-
-const BONDS_DISCRIMINATOR: [u8; 8] = [224, 128, 48, 251, 182, 246, 111, 196];
-const BONDS_PROGRAM_ID: &str = "vBoNdEvzMrSai7is21XgVYik65mqtaKXuSdMBJ1xkW4";
 
 pub async fn collect_bonds(options: CommonCollectOptions) -> anyhow::Result<()> {
     let client = &get_rpc_client(options.rpc_url, options.commitment.to_string());
@@ -20,13 +18,13 @@ pub async fn collect_bonds(options: CommonCollectOptions) -> anyhow::Result<()> 
     let epoch = current_epoch_info.epoch;
 
     let mut bonds: Vec<ValidatorBondRecord> = vec![];
-    let bonds_program = BONDS_PROGRAM_ID.try_into()?;
+    let bonds_program = PROGRAM_ID.try_into()?;
     let bond_accounts = client
         .get_program_accounts_with_config(
             &bonds_program,
             RpcProgramAccountsConfig {
                 filters: Some(vec![solana_client::rpc_filter::RpcFilterType::Memcmp(
-                    Memcmp::new_base58_encoded(0, &BONDS_DISCRIMINATOR),
+                    Memcmp::new_base58_encoded(0, &Bond::DISCRIMINATOR),
                 )]),
                 account_config: RpcAccountInfoConfig {
                     encoding: Some(UiAccountEncoding::Base64),
