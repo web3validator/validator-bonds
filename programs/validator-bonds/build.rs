169e17061ba78a81357bae57e7a175f3ef2a7b08
Ondra Chaloupka <chalda@marinade.finance>
2024-03-27 11:20:19 +0100

    [review] overflow check required in build.rs

--- programs/validator-bonds/build.rs
+++ programs/validator-bonds/build.rs
@@ -1,4 +1,19 @@
 fn main() {
+    // we require overflow checks for the program to function correctly
+    match ::std::panic::catch_unwind(|| {
+        #[allow(arithmetic_overflow)]
+        let _ = 255_u8 + 1;
+    }) {
+        Ok(_) => {
+            // `not(cfg(overflow_checks))`
+            panic!("overflow checks are required for the program to function correctly");
+        }
+        Err(_) => {
+            // `cfg(overflow_checks)`
+            println!("cargo:rustc-cfg=overflow_checks");
+        }
+    }
+
     println!("cargo:rerun-if-env-changed=GIT_REV");
     println!(
         "cargo:rustc-env=GIT_REV={}",
